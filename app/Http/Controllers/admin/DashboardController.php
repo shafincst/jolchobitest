de6e955ecabbe41fa0daabf446020020c858613a
shafin Ahmed <shafin19110@gmail.com>
2024-04-06 23:23:19 +0600

    count add dashboard cotroller

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\admin\blog;
 use App\Models\admission_form;
+use App\Models\advisor;
+use App\Models\classes;
+use App\Models\Media;
+use App\Models\testimonials;
 use Illuminate\Foundation\Auth\User as AuthUser;
 
 class DashboardController extends Controller
@@ -15,7 +19,11 @@ class DashboardController extends Controller
     public function index(){
         $count = blog::count();
         $count_user = admission_form::count();
-        return view('admin.dashboard' , compact('count','count_user'));
+        $count_course = classes::count();
+        $count_adviser = advisor::count();
+        $count_testimonial = testimonials::count();
+        $count_media = Media::count();
+        return view('admin.dashboard' , compact('count','count_user','count_course','count_adviser','count_testimonial','count_media'));
     }
     
 }
